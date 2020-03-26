efd77ba2a1cca69feab8ef4388ad9660814ca9b9
Nick Bild <nick.bild@gmail.com>
2020-03-26 12:19:56 -0400

    Initial openpose code.

---
+++ openpose.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import pyopenpose as op
+
+
+if __name__ == '__main__':
+    params = dict()
+    params["model_folder"] = "/home/nick/software/openpose/openpose/models/"
+
+    # Starting OpenPose
+    opWrapper = op.WrapperPython()
+    opWrapper.configure(params)
+    opWrapper.start()
+
+    dst = cv2.imread("test.jpg")
+
+    datum = op.Datum()
+    datum.cvInputData = dst
+    opWrapper.emplaceAndPop([datum])
+    newImage = datum.cvOutputData[:, :, :]
+    cv2.imwrite("result.jpg", newImage)
+
