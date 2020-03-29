0030dd8e00339fba4ddd0c41cd90c9739c8c027f
Nick Bild <nick.bild@gmail.com>
2020-03-28 20:55:09 -0400

    Fine tuning depth data.

--- 3dcam.py
+++ 3dcam.py
@@ -4,6 +4,7 @@ from subprocess import call
 import cv2
 import numpy as np
 import pyopenpose as op
+import os
 
 
 img_width = 1024
@@ -19,6 +20,7 @@ for output_pin in output_pins:
 
 cmds = ["scp pi@192.168.1.170:/home/pi/deepclean/pi.jpg pi1.jpg", 
         "scp pi@192.168.1.171:/home/pi/deepclean/pi.jpg pi2.jpg"]
+FNULL = open(os.devnull, 'w')
 
 opWrapper = op.WrapperPython()
 opWrapper.configure(params)
@@ -45,16 +47,13 @@ while True:
 
     # Retrieve images from Raspberry Pis.
     for cmd in cmds:
-        call(cmd.split(" "))
+        call(cmd.split(" "), stdout=FNULL)
 
-    # Rectify stereo images.
-    
-
-    # Analyze stereo images.
+    # Rectify/analyze stereo images.
     imgL = cv2.imread('pi2.jpg')
     imgR = cv2.imread('pi1.jpg')
     #stereo = cv2.StereoBM_create(numDisparities=128, blockSize=15)
-    stereo = cv2.StereoSGBM_create(numDisparities=128, blockSize=15)
+    stereo = cv2.StereoSGBM_create(numDisparities=16, blockSize=1)
     
     fixedL = cv2.remap(imgL, leftMapX, leftMapY, cv2.INTER_LINEAR)
     fixedR = cv2.remap(imgR, rightMapX, rightMapY, cv2.INTER_LINEAR)
@@ -62,18 +61,11 @@ while True:
     grayL = cv2.cvtColor(fixedL, cv2.COLOR_BGR2GRAY)
     grayR = cv2.cvtColor(fixedR, cv2.COLOR_BGR2GRAY)
 
-    stereo.setMinDisparity(5)
-    stereo.setSpeckleRange(10)
-    stereo.setSpeckleWindowSize(15)
+    stereo.setMinDisparity(-2)
+    stereo.setSpeckleRange(2)
+    stereo.setSpeckleWindowSize(5)
 
     disparity = stereo.compute(grayL, grayR)
-
-    #hv = min([sublist[-1] for sublist in disparity])
-    #print(hv)
-    #disparity = disparity * 3.1875
-
-    # View disparity map.
-    cv2.imwrite('result_distance.jpg', disparity)
 
     #cv2.imwrite('left.jpg', grayL)
     #cv2.imwrite('right.jpg', grayR)
@@ -85,15 +77,31 @@ while True:
 
     try:
         # Wrist location.
-        print("R wrist: {}".format(datum.poseKeypoints[0][4])) # Right wrist [x, y, score].
-        print("L wrist: {}".format(datum.poseKeypoints[0][7])) # Left wrist [x, y, score].
+        #print("R wrist: {}".format(datum.poseKeypoints[0][4])) # Right wrist [x, y, score].
+        #print("L wrist: {}".format(datum.poseKeypoints[0][7])) # Left wrist [x, y, score].
 
-        # Disparity at wrist locations.
-        print(disparity[int(datum.poseKeypoints[0][4][0])][int(datum.poseKeypoints[0][4][1])])
-        print(disparity[int(datum.poseKeypoints[0][7][0])][int(datum.poseKeypoints[0][7][1])])
+        # Raw / mean disparity in region.
+        if int(datum.poseKeypoints[0][4][0]) > 0:
+            print("Right ++++")
+            #print(disparity[int(datum.poseKeypoints[0][4][0])][int(datum.poseKeypoints[0][4][1])])
+            disparityLwrist = disparity[ int(datum.poseKeypoints[0][4][0])-20:int(datum.poseKeypoints[0][4][0])+20, int(datum.poseKeypoints[0][4][1])-20: int(datum.poseKeypoints[0][4][1])+20 ]
+            print(disparityLwrist.mean())
+        
+        if int(datum.poseKeypoints[0][7][0]) > 0:
+            #print("Left ----")
+            #print(disparity[int(datum.poseKeypoints[0][7][0])][int(datum.poseKeypoints[0][7][1])])
+            disparityRwrist = disparity[ int(datum.poseKeypoints[0][7][0])-20:int(datum.poseKeypoints[0][7][0])+20, int(datum.poseKeypoints[0][7][1])-20: int(datum.poseKeypoints[0][7][1])+20 ]
+            #print(disparityRwrist.mean())
+
+        # Draw circles on image.
+        disparity = cv2.circle(disparity, (int(datum.poseKeypoints[0][4][0]), int(datum.poseKeypoints[0][4][1])), 20, (255, 0, 0), 2)
+        disparity = cv2.circle(disparity, (int(datum.poseKeypoints[0][7][0]), int(datum.poseKeypoints[0][7][1])), 20, (255, 0, 0), 2)
 
     except:
         print("No human detected.")
+
+    # View disparity map.
+    cv2.imwrite('result_distance.jpg', disparity)
 
     # View annotated pose image.
     # newImage = datum.cvOutputData[:, :, :]
